a84574f0b082e010d6d67c17afcb98ebaebe4824
Jean Brigonnet <jeanbrigo@gmail.com>
2023-03-04 12:41:44 -0600

    Initial Commit

---
+++ RandomPhoto/RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoApp.swift
+//  RandomPhoto
+//
+//  Created by Jean Brigonnet on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
